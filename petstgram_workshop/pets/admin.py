6481c3e5873eaab800436443ef72b24a0a4cc7a8
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-10 21:11:34 +0200

    added photo models and fixed admin panel interface

--- petstgram_workshop/pets/admin.py
+++ petstgram_workshop/pets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from petstgram_workshop.pets.models import Pet
+
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+
+
+admin.site.register(Pet, PetAdmin)
--- petstgram_workshop/photos/models.py
+++ petstgram_workshop/photos/models.py
@@ -1,3 +1,16 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+from petstgram_workshop.pets.models import Pet
+from petstgram_workshop.photos.validators import validate_file_size
+
 # Create your models here.
+
+
+class Photo(models.Model):
+    photo = models.ImageField(validators=(validate_file_size,))
+    description = models.TextField(max_length=300, validators=(MinLengthValidator(10),), blank=True,
+                                   null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    tagged_pets = models.ManyToManyField(Pet, blank=True)
+    date_of_publication = models.DateField(auto_now=True)
